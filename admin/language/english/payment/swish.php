4367897a3689fae59734ec02885b2ebcdbcf425b
mustafa <mustafa8@live.se>
2016-11-23 13:59:51 +0100

    added admin

---
+++ admin/language/english/payment/swish.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']      = 'Swish';
+
+// Text 
+$_['text_payment']       = 'Swish';
+$_['text_success']       = 'Done: You have finally added Swish instructions!';
+$_['text_edit']          = 'Settinga in Swish module';
+
+// Entry
+$_['entry_bank']         = 'Swish Instructions:';
+$_['entry_total']        = 'Total:<br /><span class="help">Amount that customer needs to buy for before Swish will be able.</span>';
+$_['entry_order_status'] = 'Order Status:';
+$_['entry_geo_zone']     = 'Geo Zone:';
+$_['entry_status']       = 'Status:';
+$_['entry_sort_order']   = 'Sort order:';
+
+// Error
+$_['error_permission']   = 'Warning: You are not allowed to change in Swish module!';
+$_['error_bank']         = 'Swish Instructions is needed!';
+?>
---
+++ admin/language/swedish/payment/swish.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']      = 'Swish';
+
+// Text 
+$_['text_payment']       = 'Swisha';
+$_['text_success']       = 'Klart: Du har ändrat i swish inställningarna!';
+$_['text_edit']          = 'Inställningar i Swish modulen';
+
+// Entry
+$_['entry_bank']         = 'Swish Instruktioner: Ange ert telefonnummer eller företagsnummer här';
+$_['entry_total']        = 'Totalt:<br /><span class="help">Summa som ordern måste uppgå till för att Swish ska visas för kunden.</span>';
+$_['entry_order_status'] = 'Order Status:';
+$_['entry_geo_zone']     = 'Geo Zone:';
+$_['entry_status']       = 'Status:';
+$_['entry_sort_order']   = 'Sorteringsordning:';
+
+// Error
+$_['error_permission']   = 'Varning: Du har inte tillåtelse att ändra i Swish modulen!';
+$_['error_bank']         = 'Swish Instruktioner Krävs!';
+?>
